78ccd7fef0dc876b9952422b7d7c264ecd3f1d6e
AyaanmandeliA <78735976+AyaanmandeliA@users.noreply.github.com>
2022-01-17 16:00:22 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,30 +2,29 @@
 NoseX = 0;
 NoseY = 0;
 
-function preload() {
- clown_nose=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png");
+function preload() { 
+    clown_nose=loadImage(" https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup() {
     canvas = createCanvas(400, 300);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300, 300);
+    video.size(400, 300);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLooaded);
     poseNet.on('pose', gotPoses);
 }
 function draw() {
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 400, 300);
     //fill(255,0,0);
     //stroke(255,0,0);
     //circle(NoseX , NoseY , 20 , 20);
-    image(clown_nose , NoseX-10 , NoseY-10 , 20 , 20)
-
+    image(clown_nose , NoseX-10 , NoseY-5 , 30 , 30)
 }
 
 function takemewithyou() {
-    save("clown.png");
+    save("m.jpg");
 }
 
 function modelLooaded() {
@@ -43,4 +42,3 @@ function gotPoses(results)
         console.log( " nose y =" + NoseY);
     }
 }
-
